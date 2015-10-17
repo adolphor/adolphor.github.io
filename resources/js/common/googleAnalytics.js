c7a5ae6bc1edec6f8eadde67174aa1f1851397c5
Bob <0haizhu0@gmail.com>
2015-10-17 16:17:05 +0800

    网站统计分析工具

---
+++ resources/js/common/googleAnalytics.js
@@ -0,0 +1,30 @@
+// 谷歌网站访问统计工具
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+// xx是统计ID
+ga('create', 'UA-68980203-1', 'auto');
+ga('send', 'pageview');
+
+
+// Github自动生成的谷歌统计代码
+//var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+//document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+//try {
+//    var pageTracker = _gat._getTracker("");
+//    pageTracker._trackPageview();
+//} catch (err) {
+//    cosnole.log(err);
+//}
+
+
+
