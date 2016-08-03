8036038f09baaffc0c58f7514a60d0578b5bccfe
Bob <0haizhu0@gmail.com>
2016-08-03 09:03:39 +0800

    生成文章结构导航

--- js/main.js
+++ js/main.js
@@ -12,18 +12,32 @@ function heightFunctions() {
 
 // 生成左侧文章结构目录
 function geneMenu() {
+    var section = '<section class="left-section "> <div class="row"><div class="left-title">' +
+        ' 文章导航 </div> </div> <ul class="left-ul">';
+
     var height = $(".post-content").height();
     if (height > 800) {
+        var pathname = window.location.pathname;
         var h2Array = $(".post-side h2");
         $.each(h2Array, function () {
-            var h2 = $(this).text();
-            console.log(h2);
+            var h2Title = $(this).text();
+            var h2Id = $(this).attr("id");
+            var link = '<li><a href="' + pathname +'#'+ h2Id + '">' + h2Title + '</a></li>'
+            section += link;
+            console.log(link);
 
             var h3Array = $(this).nextUntil('h2').filter("h3");
             $.each(h3Array, function () {
-                var h3 = $(this).text();
-                console.log(h3);
+                var h3Title = $(this).text();
+                var h3Id = $(this).attr("id");
+                var subLink = '<li><a href="' + pathname +'#'+ h3Id + '">' + h3Title + '</a></li>'
+                section += subLink;
+                console.log(subLink);
             });
         });
+        section+=("</ul></section>");
+
+       $(".left-side section:last-child").append(section);
+
     }
 }
