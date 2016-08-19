24a5829dc4610a9ef2de32ae3097ffece475e729
Bob <0haizhu0@gmail.com>
2016-08-19 11:51:15 +0800

    20160818

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Push to gitHub ..."
+git push origin gh-pages
+
+echo "Push to osChina ..."
+git push oschina master
+
+
+# git@github.com:NorthFacing/adolphor.git
+# git@git.oschina.net:adolphor/northfacing.github.io.git
