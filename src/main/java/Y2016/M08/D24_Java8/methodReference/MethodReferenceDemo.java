76bcda095ee4011f6fbea3d7e9c24c821a8ea2b2
Bob <0haizhu0@gmail.com>
2016-08-27 00:07:43 +0800

    改引用方法的接收类使用Java8内置类

--- src/main/java/Y2016/M08/D24_Java8/methodReference/MethodReferenceDemo.java
+++ src/main/java/Y2016/M08/D24_Java8/methodReference/MethodReferenceDemo.java
@@ -3,6 +3,9 @@ package Y2016.M08.D24_Java8.methodReference;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * Created by Bob on 2016/8/24.
@@ -14,63 +17,48 @@ public class MethodReferenceDemo {
      * 范例1
      */
     // lambda表达式：作为函数式接口实现参考上节范例
-    Converter<String, Integer> converter = Integer::valueOf;  // 方法引用：调用静态方法，作为函数式接口实现
-    Integer i = converter.convert("123");
-    System.out.println(i);
+    Function<String, Integer> converter1 = num -> Integer.valueOf(num);  // lambda表达式方式
+    Integer i1 = converter1.apply("123");
+    System.out.println(i1);
+    Function<String, Integer> converter2 = Integer::valueOf;  // 方法引用：调用静态方法，作为函数式接口实现
+    Integer i2 = converter2.apply("123");
+    System.out.println(i2);
 
     /**
      * 范例2
      */
     List<String> names = Arrays.asList("Mahesh", "Suresh", "Ramesh", "Naresh", "Kalpesh");
     Collections.sort(names, (a, b) -> a.compareTo(b)); // lambda表达式进行排序
-    names.forEach((name) -> System.out.println(name));    // 前面讲过，lambda表达式方式进行遍历的方式
+    names.forEach((name) -> System.out.println(name));    // lambda表达式方式进行遍历的方式
     names.forEach(System.out::println); // 方法引用：调用系统静态方法进行遍历
 
     /**
      * 范例3
      */
-    PersonFactory<Person> factory1 = (name, age) -> new Person(); // lambda表达式
-    Person person1 = factory1.create("Person1", 28);
+    BiFunction<String, Integer, Person> biFunction1 = (name, age) -> new Person(name, age); // lambda表达式
+    Person person1 = biFunction1.apply("Person1", 28);
     person1.eat();
 
-    PersonFactory<Person> factory2 = Person::new;  // 方法引用：构造函数
-    Person person2 = factory2.create("Person2", 28);
-    person2.eat();
+    BiFunction<String, Integer, Person> biFunction2 = Person::new;  // 方法引用：构造函数
+    Person person2 = biFunction2.apply("Person2", 28);
+    person2.run(100);
 
     /**
      * 范例4
      */
     Something something = new Something();
-    OperateUtil<String, String> startsUtil = something::startsWith;
-    OperateUtil<String, String> endsUtil = something::endsWith;
-    TestOperateUtil testOperate = new TestOperateUtil();
-    System.out.println("Java starts with: " + testOperate.oerateResult(startsUtil, "Java"));
-    System.out.println("Java ends with: " + testOperate.oerateResult(endsUtil, "Java"));
-    // 相当于此接口的实现类调用自身的方法用作函数式接口的实现，只是这里的实现类和函数式接口之间并没有继承关系，
-    // 只要符合参数类型，任何函数式接口都可以作为此实例方法的引用，而函数式接口实例对此实例方法本身没有任何影响
-    // 比如，此实例方法甚至可以使用 Converter 函数式接口进行接收和引用
-    Converter<String, String> startsWith = something::startsWith;
-    // 只不过换了一个方法名，也就是将一个实例方法抽象化为另一个接口类型的实现了
-    System.out.println("Java starts with: " + startsWith.convert("Java"));
+    UnaryOperator<String> starts = something::startsWith;
+    UnaryOperator<String> ends = something::endsWith;
+    TestFunction testFun = new TestFunction();
+    System.out.println("Java starts with: " + testFun.funResult(starts, "Java"));
+    System.out.println("Java ends with: " + testFun.funResult(ends, "Java"));
 
   }
-}
-
-@FunctionalInterface
-interface Converter<V, T> {
-  T convert(V v);
 }
 
 class Person {
   public String name;
   public int age;
-
-  Person() {
-  }
-
-  Person(String name) {
-    this.name = name;
-  }
 
   Person(String name, int age) {
     this.name = name;
@@ -87,25 +75,9 @@ class Person {
 
 }
 
-@FunctionalInterface
-interface PersonFactory<P extends Person> {
-  P create(String name, int age);
-}
-
-/**
- * 泛型化的函数式接口
- *
- * @param <T> 参数类型
- * @param <V> 返回值类型
- */
-@FunctionalInterface
-interface OperateUtil<T, V> {
-  V operate(T t);
-}
-
-class TestOperateUtil {
-  public Object oerateResult(OperateUtil operateUtil, String str) {
-    return operateUtil.operate(str);
+class TestFunction {
+  public Object funResult(Function function, String str) {
+    return function.apply(str);
   }
 }
 
