016c01fe5ef3c67674f9e4a456034be275eca1bf
Bob <0haizhu0@gmail.com>
2016-09-08 10:10:15 +0800

    笔误修正

--- src/main/java/Y2016/M08/D08_AbstractCollection/toArray/ToArrayDemo.java
+++ src/main/java/Y2016/M08/D08_AbstractCollection/toArray/ToArrayDemo.java
@@ -5,7 +5,7 @@ package Y2016.M08.D08_AbstractCollection.toArray;
  * Created by Bob on 2016/8/10.
  * 即便finishToArray方法能够将后续变动的元素追加到数组，
  * 但是因为不是线程安装，所以结果并没有保障。如果需要线程
- * 安装个，请使用Concurrent Collections。
+ * 安全，请使用Concurrent Collections。
  */
 public class ToArrayDemo {
   public static void main(String[] args) {
