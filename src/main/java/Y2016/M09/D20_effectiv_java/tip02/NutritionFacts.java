a3b4952820d9b93c2da64682b113ed852f83a8c6
Bob <0haizhu0@gmail.com>
2017-01-18 13:37:11 +0800

    代码引用改为连接形式

--- src/main/java/Y2016/M09/D20_effectiv_java/tip02/NutritionFacts.java
+++ src/main/java/Y2016/M09/D20_effectiv_java/tip02/NutritionFacts.java
@@ -2,6 +2,8 @@ package Y2016.M09.D20_effectiv_java.tip02;
 
 /**
  * Created by Bob on 2017/1/18.
+ * 引用地址：
+ * http://adolphor.com/blog/2016/09/20/effective-java-second-edition.html#tips2
  */
 public class NutritionFacts {
   private final int servingSize;
@@ -17,24 +19,27 @@ public class NutritionFacts {
     private int calories = 0;
     private int fat = 0;
 
-    public Builder(int servingSize, int servings) {
+    public Builder(int servingSize, int servings) { // 必填属性构造器
       this.servingSize = servingSize;
       this.servings = servings;
     }
-    public Builder calories(int val) {
-      calories = val;
+
+    public Builder calories(int val) { // 可选属性set方法
+      this.calories = val;
       return this;
     }
+
     public Builder fat(int val) {
       this.fat = val;
       return this;
     }
 
-    public NutritionFacts build() {
+    public NutritionFacts build() { // 实例化目标对象
       return new NutritionFacts(this);
     }
   }
-  public NutritionFacts(Builder builder) {
+
+  public NutritionFacts(Builder builder) { // 供内部类构造方法调用
     servingSize = builder.servingSize;
     servings = builder.servings;
     calories = builder.calories;
