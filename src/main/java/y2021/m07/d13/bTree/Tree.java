e617e1c87909aaad665e7fbedc7a1c514f9ca4bf
Bob <0haizhu0@gmail.com>
2021-07-16 14:13:14 +0800

    二叉树

--- src/main/java/y2021/m07/d13/bTree/Tree.java
+++ src/main/java/y2021/m07/d13/bTree/Tree.java
@@ -53,19 +53,30 @@ public class Tree {
     }
   }
 
-  public void deleted(int d) {
-
-  }
-
-  public void display() {
-    inOrder(root);
+  public void display(int traverseType) {
+    switch (traverseType) {
+      case 1:
+        System.out.println("\nPreOrder traversal:");
+        preOrder(root);
+        break;
+      case 2:
+        System.out.println("\nInOrder traversal:");
+        inOrder(root);
+        break;
+      case 3:
+        System.out.println("\nPostOrder traversal:");
+        postOrder(root);
+        break;
+      default:
+        System.err.println("error");
+    }
   }
 
   /**
    * 中序遍历：A*(B+C)
    */
-  private void inOrder(Node localRoot){
-    if (localRoot!=null){
+  private void inOrder(Node localRoot) {
+    if (localRoot != null) {
       inOrder(localRoot.leftChild);
       System.out.println(localRoot);
       inOrder(localRoot.rightChild);
@@ -75,8 +86,8 @@ public class Tree {
   /**
    * 前序遍历：*A+BC
    */
-  private void preOrder(Node localRoot){
-    if (localRoot!=null){
+  private void preOrder(Node localRoot) {
+    if (localRoot != null) {
       System.out.println(localRoot);
       inOrder(localRoot.leftChild);
       inOrder(localRoot.rightChild);
@@ -86,12 +97,107 @@ public class Tree {
   /**
    * 后序遍历：ABC+*
    */
-  private void postOrder(Node localRoot){
-    if (localRoot!=null){
+  private void postOrder(Node localRoot) {
+    if (localRoot != null) {
       inOrder(localRoot.leftChild);
       inOrder(localRoot.rightChild);
       System.out.println(localRoot);
     }
+  }
+
+  /**
+   * 删除有三种场景：
+   * 1. 叶结点：没有子节点
+   * 2. 一个子节点
+   * ├── 2.1 左子节点
+   * ├── 2.2 右子节点
+   * 3. 两个子节点
+   */
+  public boolean deleted(int key) {
+    Node current = root;
+    Node parent = root;
+    boolean isLeftChild = true;
+    // 0. 先找需要删除的节点
+    // ├── 要么找到
+    // ├── 要么返回false
+    while (current.iData != key) {
+      parent = current;
+      if (key < current.iData) {
+        isLeftChild = true;
+        current = current.leftChild;
+      } else {
+        isLeftChild = false;
+        current = current.rightChild;
+      }
+      if (current == null) {
+        return false;
+      }
+    }
+    // 1. 没有子节点
+    if (current.leftChild == null && current.rightChild == null) {
+      if (current == root) {
+        root = null;
+      } else if (isLeftChild) {
+        parent.leftChild = null;
+      } else {
+        parent.rightChild = null;
+      }
+    }
+    // 2. 一个子节点：子节点左边的肯定也在父节点左边，子节点右边的肯定也在父节点右边
+    // ├── 2.1 左子节点
+    else if (current.rightChild == null) {
+      if (current == root) {
+        root = current.leftChild;
+      }
+      // 把仅有的一个子节点挂载到父节点上
+      else if (isLeftChild) {
+        parent.leftChild = current.leftChild;
+      } else {
+        parent.rightChild = current.leftChild;
+      }
+    }
+    // ├── 2.2 右子节点
+    else if (current.leftChild == null) {
+      if (current == root) {
+        root = current.rightChild;
+      } else if (isLeftChild) {
+        parent.leftChild = current.rightChild;
+      } else {
+        parent.rightChild = current.rightChild;
+      }
+    }
+    // 3. 两个子节点
+    // 需要找到需要删除的节点的后继节点(successor)：右子节点的左子节点的左子节点一直找下去
+    else {
+      Node successor = getSuccessor(current);
+      if (current == root) {
+        root = successor;
+      } else if (isLeftChild) {
+        parent.leftChild = successor;
+      } else {
+        parent.rightChild = successor;
+      }
+      successor.leftChild = current.leftChild;
+    }
+    return true;
+  }
+
+  public Node getSuccessor(Node delNode) {
+    Node successorParent = delNode;
+    Node successor = delNode;
+    Node current = delNode.rightChild;
+    // go to right child until no more left children
+    while (current != null) {
+      successorParent = successor;
+      successor = current;
+      current = current.leftChild;
+    }
+    // if successor not right child, make connections
+    if (successor != delNode.rightChild) {
+      successorParent.leftChild = successor.rightChild;
+      successor.rightChild = delNode.rightChild;
+    }
+    return successor;
   }
 
 }
--- src/main/java/y2021/m07/d13/bTree/TreeApp.java
+++ src/main/java/y2021/m07/d13/bTree/TreeApp.java
@@ -10,14 +10,26 @@ public class TreeApp {
     theTree.insert(50, 1.5);
     theTree.insert(25, 1.7);
     theTree.insert(75, 1.9);
+    theTree.insert(10, 1.0);
+    theTree.insert(99, 1.8);
 
     Node found = theTree.find(30);
-    theTree.display();
     if (found != null) {
       System.out.println("Found the node with key 25");
     } else {
       System.out.println("Could not find node with key 25");
     }
+
+    theTree.display(1);
+    theTree.display(2);
+    theTree.display(3);
+
+    boolean deleted = theTree.deleted(25);
+    System.out.println("delete:" + deleted);
+
+    theTree.display(1);
+    theTree.display(2);
+    theTree.display(3);
 
   }
 }
